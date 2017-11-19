919a5a91c7d792db144f0ad43ac4462982e7cb05
Łukasz <pusz.lukasz@gmail.com>
2017-11-19 18:54:00 +0100

    Added back button action handling, IBOutlets

--- pokedex/controller/PokemonDetailVC.swift
+++ pokedex/controller/PokemonDetailVC.swift
@@ -9,6 +9,19 @@
 import UIKit
 
 class PokemonDetailVC: UIViewController {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var defenceLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var pokedexLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var attackLabel: UILabel!
+    @IBOutlet weak var currentEvolutionImage: UIImageView!
+    @IBOutlet weak var nextEvolutionImage: UIImageView!
+    @IBOutlet weak var evolutionLabel: UILabel!
+    
     private var _pokemon: Pokemon!
     
     var pokemon: Pokemon {
@@ -23,4 +36,9 @@ class PokemonDetailVC: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func onBackButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
